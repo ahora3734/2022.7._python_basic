221c1cf6bac5fc9bfcd92002423c0bb7c5a5d445
ahora3734 <ahora3734@gmail.com>
2022-08-02 14:19:47 +0900

    update

---
+++ 00_295_기본자료형과 자료구조.py
@@ -0,0 +1,7 @@
+#====p.295====
+company = '네이버'
+print(company)
+
+ip = '223.130.200.107' #도메인으로 ip 확인(https://thisthatbase.com/domain-ip-check-online-command/)
+url = 'naver.com'
+print("네이버 url(IP) : ", url +"(" + ip + ")")
---
+++ 00_297_dictionary.py
@@ -0,0 +1,30 @@
+data = { "query" : "진연"}    #dictionary는 { } 로 표시 {key : value} 
+
+print("data :", data)                           # data : {'query': '진연'}
+print("type(data) :", type(data))               # type(data) : <class 'dict'>
+
+print("data['query'] :", data['query'])         # data['query'] : 진연
+
+print("data.get('query') :", data.get('query')) # data.get('query') : 진연
+
+print("============")
+
+mydata = {"이름" : "김진연", "직업" : "기술교사", "별자리" : "물고기자리"}  
+print("mydata :", mydata)                           # mydata : {'이름': '김진연', '직업': '기술교사', '별자리': '물고기자리'}
+print("mydata['이름'] :", mydata['이름'])            # mydata['이름'] : 김진연
+print("mydata.get('이름') :", mydata.get('이름'))    # mydata.get('이름') : 김진연
+
+print("============")
+
+myfamily = {"아빠" : {"이름" : "김진연", "직업" : "기술교사", "별자리" : "물고기자리"},
+            "엄마" : {"이름" : "양정화", "직업" : "유치원교사", "별자리" : "양자리"},  
+            "큰아들" : {"이름" : "김민수", "직업" : "초등학생", "별자리" : "사자자리"},  
+            "작은아들" : {"이름" : "김민수", "직업" : "어린이집원생", "별자리" : "황소자리"}  }
+
+print("myfamily :", myfamily)                                                # myfamily : {'아빠': {'이름': '김진연', '직업': '기술교사', '별자리': '물고기자리'}, 
+                                                                             #               '엄마': {'이름': '양정화', '직업': '유치원교사', '별자리': '양자리'}, 
+                                                                             #               '큰아들': {'이름': '김민수', '직업': '초등학생', '별자리': '사자자리'}, 
+                                                                             #               '작은아들': {'이름': '김민수', '직업': '어린이집원생', '별자리': '황소자리'}}
+print("mydata['아빠'] :", myfamily['아빠'])                                   # mydata['아빠'] : {'이름': '김진연', '직업': '기술교사', '별자리': '물고기자리'}
+print("mydata['아빠']['직업'] :", myfamily['아빠']['직업'])                    # mydata['아빠']['직업'] : 기술교사
+print("mydata.get('아빠').get('직업') :", myfamily.get('아빠').get('직업'))    # mydata.get('아빠').get('직업') : 기술교사
