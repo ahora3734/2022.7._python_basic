7ef92e2be3b4f5ed71ad260199fc6dac7488b16e
ahora3734 <ahora3734@gmail.com>
2022-08-02 14:22:00 +0900

    up

--- 00_295_기본자료형과 자료구조.py
+++ 00_295_기본자료형과 자료구조.py
@@ -4,4 +4,4 @@ print(company)
 
 ip = '223.130.200.107' #도메인으로 ip 확인(https://thisthatbase.com/domain-ip-check-online-command/)
 url = 'naver.com'
-print("네이버 url(IP) : ", url +"(" + ip + ")")
+print("네이버 url(IP): ", url +"(" + ip + ")") #문자열을 합칠 때는 +
